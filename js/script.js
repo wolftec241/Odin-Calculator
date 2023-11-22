d708ba6cf5cdc88f519cf6add09b439dbd57d55e
Igor Golovko <2003igor09@gmail.com>
2023-11-22 21:16:12 +0200

    Adding basic html and css

---
+++ js/script.js
@@ -0,0 +1,29 @@
+//All imports of output elements
+const func = document.getElementById("func");
+const result = document.getElementById("result");
+
+
+function add(num1, num2){
+    return num1+num2;
+}
+
+function subtract(num1, num2){
+    return num1-num2;
+}
+
+function multiply(num1, num2){
+    return num1*num2;
+}
+
+function divide(num1, num2){
+    return num1/num2;
+}
+
+
+
+
+function main(){   
+    
+}
+
+main();
