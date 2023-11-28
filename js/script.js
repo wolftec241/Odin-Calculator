2dadfd6350f9ab0a45a8badfb6d1ba929b652afe
Igor Golovko <2003igor09@gmail.com>
2023-11-28 17:54:01 +0200

    Adding functions for working with parentheses

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@
 const func = document.getElementById("func");
 const result = document.getElementById("result");
 
-let arrFunc = [];
+let arrFunc = []; //Array the will contain the function that user make 
+
+
 
 function add(num1, num2){
     return num1+num2;
@@ -27,7 +29,7 @@ function divide(num1, num2) {
 function restartCalc(){
     arrFunc = [];
     func.innerText = '0';
-    result.innerText = '';
+    result.innerText = '0';
 }
 
 function changeCalc(funcNum, resultNum){
@@ -37,13 +39,23 @@ function changeCalc(funcNum, resultNum){
 }
 
 
-function buttonPress(value){
+function buttonPress(value){4
     switch(value){
-        case "AC":
+        case "AC": //restart the calculator
             restartCalc();
+            break;
+
+        case "C": //Backspace last element
+            backspace();
+            break;
 
         case "=": 
-            calculateResult();
+            calcResult();
+            break;
+
+        case "(":
+        case ")":
+            handleParentheses(value);
             break;
         
         //All other operators
@@ -51,18 +63,18 @@ function buttonPress(value){
         case "-":
         case "×":
         case "÷":
-            if (!isOperator(arrFunc[arrFunc.length - 1])) {
+            if (!isOperator(arrFunc[arrFunc.length - 1])
+                && arrFunc.length > 0) {
                 //Check if last element is number
                 if(arrFunc[arrFunc.length - 1] === "="){
                     arrFunc.pop();
-                    func.innerText = arrFunc[0];
                 }
                 arrFunc.push(value);
                 func.innerText += value;
                 console.log("a")
             }
             break;
-        default:
+        default: //If it number
             if(arrFunc[arrFunc.length - 1] === "=")
                     restartCalc();
             
@@ -87,44 +99,140 @@ function isOperator(value) {
     return ["+", "-", "×", "÷"].includes(value);
 }
 
+function backspace() {
+    let currentText = func.innerText;
+    func.innerText = currentText.slice(0, -1);
 
-function calculateResult() {
+    let lastIndex = arrFunc.length - 1;
+    if(arrFunc[lastIndex] === "=")
+        restartCalc();
+    if(arrFunc[lastIndex] >= 10)
+        arrFunc[lastIndex] = (arrFunc[lastIndex]/10).getFixed(0);
+    else
+        arrFunc.pop();
+}
+
+function handleParentheses(value) {
+    if (value === "(") {
+        // Handle open parenthesis
+        if (arrFunc.length === 0 || 
+            isOperator(arrFunc[arrFunc.length - 1])) {
+            // If the expression is empty or the last element is an operator, add the open parenthesis
+            arrFunc.push("[");
+            func.innerText += value;
+        }
+    }
+    else if (value === ")") {
+        // Handle close parenthesis
+        if (arrFunc.length > 0 && 
+            typeof arrFunc[arrFunc.length - 1] === "number") {
+            // If the last element is a number, add the close parenthesis
+            arrFunc.push("]");
+            func.innerText += value;
+        }
+    }
+}
+
+function calcResult() {
     if (arrFunc.length >= 3 
-        && typeof arrFunc[arrFunc.length - 1] === "number") {
-            //First, solve all multiply and dividing
-            for(let i = 0; i < arrFunc.length; i++){
-                if(arrFunc[i] == "×"){
-                    arrFunc[i+1] *= arrFunc[i-1];
-                    arrFunc.splice(i-1, 2);
-                    i--;
-                }
-                if(arrFunc[i] == "÷"){
-                    arrFunc[i+1] = divide(arrFunc[i-1], arrFunc[i+1]);
-                    arrFunc.splice(i-1, 2);
-                    i--;
-                }
-            }
-            
-            //Secondly, solve all adding and subtracting
-            for(let i = 0; i < arrFunc.length; i++){
-                if(arrFunc[i] == "+"){
-                    arrFunc[i+1] += arrFunc[i-1];
-                    arrFunc.splice(i-1, 2);
-                    i--;
-                }
-                if(arrFunc[i] == "-"){
-                    arrFunc[i+1] -= arrFunc[i-1];
-                    arrFunc.splice(i-1, 2);
-                    i--;
-                }
-            }
+        && (typeof arrFunc[arrFunc.length - 1] === "number")
+        || arrFunc[arrFunc.length - 1] === "]") {
+
+            resolveParentheses();            
+
+            arrFunc = calculateExpression(arrFunc);
+            //Returning calculation in [] for adding in the end =
 
             //Change the output of calculator and in arrFunc
             console.table(arrFunc);
+            func.textContent = arrFunc[0];
             result.textContent = arrFunc[0];
             arrFunc.push("=");
-    }   
+    }
+    else{
+        result.textContent = "Error";
+    }
 }
+
+//solve the parentheses 
+function resolveParentheses() {
+    let openIndex = arrFunc.lastIndexOf("[");
+    let closeIndex = arrFunc.indexOf("]", openIndex);
+
+    while (openIndex !== -1 && closeIndex !== -1) {
+        // Extract the expression inside parentheses
+        let subExpression = arrFunc.slice(openIndex + 1, closeIndex);
+
+        // Recursively resolve nested parentheses in the subExpression
+        resolveNestedParentheses(subExpression);
+
+        // Calculate the result of the subExpression
+        let subResult = calculateExpression(subExpression)[0];
+
+        // Replace the subExpression with its result
+        arrFunc.splice(openIndex, closeIndex - openIndex + 1, subResult);
+
+        // Find the next innermost set of parentheses
+        openIndex = arrFunc.lastIndexOf("[");
+        closeIndex = arrFunc.indexOf("]", openIndex);
+    }
+}
+
+//Solve nested parentheses
+function resolveNestedParentheses(expression) {
+    let nestedOpenIndex = expression.lastIndexOf("[");
+    let nestedCloseIndex = expression.indexOf("]", nestedOpenIndex);
+
+    while (nestedOpenIndex !== -1 && nestedCloseIndex !== -1) {
+        let nestedExpression = expression.slice(nestedOpenIndex + 1, nestedCloseIndex);
+
+        // Recursively resolve nested parentheses in the nested expression
+        resolveNestedParentheses(nestedExpression);
+
+        // Calculate the result of the nested expression
+        let nestedResult = calculateExpression(nestedExpression)[0];
+
+        // Replace the nested expression with its result in the main array (arrFunc)
+        expression.splice(nestedOpenIndex, nestedCloseIndex - nestedOpenIndex + 1, nestedResult);
+
+        // Find the next innermost set of nested parentheses
+        nestedOpenIndex = expression.lastIndexOf("[");
+        nestedCloseIndex = expression.indexOf("]", nestedOpenIndex);
+    }
+}
+
+function calculateExpression(expression) {
+    //First, solve all multiply and dividing
+    for(let i = 0; i < expression.length; i++){
+        if(expression[i] === "×"){
+            expression[i+1] *= expression[i-1];
+            expression.splice(i-1, 2);
+            i--;
+        }
+        if(expression[i] === "÷"){
+            expression[i+1] = divide(expression[i-1], expression[i+1]);
+            expression.splice(i-1, 2);
+            i--;
+        }
+    }
+    
+    //Secondly, solve all adding and subtracting
+    for(let i = 0; i < expression.length; i++){
+        if(expression[i] === "+"){
+            expression[i+1] += expression[i-1];
+            expression.splice(i-1, 2);
+            i--;
+        }
+        if(expression[i] === "-"){
+            expression[i+1] -= expression[i-1];
+            expression.splice(i-1, 2);
+            i--;
+        }
+    }
+
+    return expression; //return the result in []
+}
+
 
 function main(){   
     
